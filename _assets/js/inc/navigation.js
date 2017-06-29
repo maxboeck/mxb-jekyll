0fafb31a8b5969e06ee1e6943e7a9a2989ba482e
Max Böck <hello@mxb.at>
2017-06-29 14:02:13 +0200

    add proper tab handling in mobile nav

--- _assets/js/inc/navigation.js
+++ _assets/js/inc/navigation.js
@@ -1,7 +1,9 @@
 import Util from './util';
 
 const SELECTORS = {
+  nav: '#nav',
   menu: '#menu',
+  navLink: '.nav__link',
   toggleBtn: '#menu-toggle',
   animationCircle: '#animation-circle',
 }
@@ -15,16 +17,18 @@ export default class Navigation {
   constructor() {
     this.isMenuOpen = false;
 
-    this.menu = document.querySelector(SELECTORS.menu);
-    this.toggleBtn = document.querySelector(SELECTORS.toggleBtn);
+    this.nav = document.querySelector(SELECTORS.nav);
+    this.menu = this.nav.querySelector(SELECTORS.menu);
+    this.toggleBtn = this.nav.querySelector(SELECTORS.toggleBtn);
+    this.navLinks = this.menu.querySelectorAll(SELECTORS.navLink);
     this.animationCircle = document.querySelector(SELECTORS.animationCircle);
 
-    this.toggleBtn.addEventListener('click', e => this.toggleMenu(e));
+    this.toggleBtn.addEventListener('click', () => this.toggleMenu());
+    this.nav.addEventListener('keydown', e => this.handleTabPress(e));
     window.addEventListener('throttledResize', () => this.setCircleSize());
   }
 
-  toggleMenu(e) {
-    e.preventDefault();
+  toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
 
     window.setTimeout(() => {
@@ -43,5 +47,35 @@ export default class Navigation {
     this.animationCircle.style.height = `${diameter * 2}px`;
     this.animationCircle.style.top = `${-diameter}px`;
     this.animationCircle.style.left = `${-diameter}px`;
+  }
+
+  handleTabPress(e) {
+    if (!this.isMenuOpen || e.ctrlKey || e.metaKey || e.altKey) {
+      return;
+    }
+
+    const lastLink = this.navLinks[(this.navLinks.length - 1)];
+    switch (e.keyCode) {
+      case 27 : // ESC
+
+        this.toggleMenu();
+        this.toggleBtn.focus();
+        break;
+
+      case 9 : // TAB
+
+        if (e.shiftKey) {
+          if (document.activeElement === this.toggleBtn) {
+            lastLink.focus();
+            e.preventDefault();
+          }
+        } else if (document.activeElement === lastLink) {
+          this.toggleBtn.focus();
+          e.preventDefault();
+        }
+        break;
+
+      default : break;
+    }
   }
 }
